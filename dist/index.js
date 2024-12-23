86ee6fab6ae3fee6224379ba09b1dd62c9992fdc
Hector <hectikorf@gmail.com>
2024-12-23 00:43:54 -0500

    Learnig to config

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let mensaje = "Hola Mundaco";
+mensaje = "Yeny";
+console.log(mensaje);
+console.warn(mensaje);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let mensaje:string = "Hola Mundaco"
+mensaje="Yeny"
+
+console.log(mensaje);
+console.warn(mensaje);
